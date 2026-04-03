ddeb15bcbdfeeb4fecfdff4a120738578d4681a4
vibha <vibhamishra0907@gmail.com>
2026-04-03 18:01:18 +0530

    build errors resolved.

--- my-app/src/app/sign-up/sso-callback/page.tsx
+++ my-app/src/app/sign-up/sso-callback/page.tsx
@@ -4,4 +4,5 @@ export default function SSOCallbackPage() {
   return (
     <AuthenticateWithRedirectCallback signUpFallbackRedirectUrl="/onboarding" />
   );
+  return <AuthenticateWithRedirectCallback signUpFallbackRedirectUrl="/onboarding" signInFallbackRedirectUrl="/onboarding" />;
 }
