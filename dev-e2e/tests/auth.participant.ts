f570756e8149785a4498869ce2b905d6a897134c
EnigmaByte7 <saxenay117@gmail.com>
2026-04-04 02:59:49 +0530

    fix: updated tests

--- dev-e2e/tests/auth.participant.ts
+++ dev-e2e/tests/auth.participant.ts
@@ -1,7 +1,9 @@
 import { test as setup, expect } from '@playwright/test';
 
+setup.describe.configure({timeout: 180000})
+
 setup('auth participant', async ({ page }) => {
-  await page.goto('/', {waitUntil:'domcontentloaded'});
+  await page.goto('/', {waitUntil:'networkidle'});
   await page.waitForTimeout(3000)
 
   await page.getByRole('button', { name: 'Sign In' }).click();
@@ -9,5 +11,7 @@ setup('auth participant', async ({ page }) => {
   await page.getByRole('button', { name: 'Continue', exact: true }).click();
   await page.getByRole('textbox', { name: 'Password' }).fill('SAFESTpass');
   await page.getByRole('button', { name: 'Continue', exact: true }).click();
+  await page.waitForTimeout(5000)
+
   await page.context().storageState({ path: 'storageState.participant.json' });
 });
